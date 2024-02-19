3c04d0b8b8bc698812162da843b173ce14509bfb
Carlos A. Luna Martinez <98297146+charlitos19@users.noreply.github.com>
2024-02-18 19:46:01 -0500

    Primer commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prueba | Upqroo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+<div id="header">
+        <div style="float: left;">UPQROO</div>
+        <div style="text-align: center;">Sistemas</div>
+    </div>
+    <div id="container">
+        <h2 id="title">Registro de Archivos</h2>
+        <form action="#" method="post">
+            <label for="nombre_archivo">Nombre del archivo:</label><br>
+            <input type="text" id="nombre_archivo" name="nombre_archivo"><br>
+            <label for="ubicacion">Ubicación:</label><br>
+            <select id="ubicacion" name="ubicacion">
+                <option value="ubicacion1">Ubicación 1</option>
+                <option value="ubicacion2">Ubicación 2</option>
+                <option value="ubicacion3">Ubicación 3</option>
+            </select><br>
+            <label for="anio">Año:</label><br>
+            <input type="number" id="anio" name="anio" min="1900" max="2099" step="1" value="2024"><br>
+            <label for="trimestre">Trimestre:</label><br>
+            <select id="trimestre" name="trimestre">
+                <option value="1">Primer Trimestre</option>
+                <option value="2">Segundo Trimestre</option>
+                <option value="3">Tercer Trimestre</option>
+            </select><br>
+            <input type="submit" value="Actualizar">
+        </form>
+    </div>
+
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+</body>
+</html>
