243d7d1b659427f72624ee4d1c64e0c6ed9fa8d1
Carlos A. Luna Martinez <98297146+charlitos19@users.noreply.github.com>
2024-02-19 22:38:44 -0500

    Modal

--- index.php
+++ index.php
@@ -33,7 +33,29 @@
             </select><br>
             <input type="submit" value="Actualizar">
 
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa omnis sed libero, a itaque et ea odit fugit consequuntur beatae error non, rem animi, sit similique necessitatibus saepe debitis quos?</p>
+            <!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+  Launch static backdrop modal
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Understood</button>
+      </div>
+    </div>
+  </div>
+</div>
 
         </form>
     </div>
